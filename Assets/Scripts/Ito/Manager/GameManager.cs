5a0335b8f8383ea4d91a5530cffc11070fab7294
伊東聖矢 <tvedaamamesab@gmail.com>
2022-01-29 20:52:53 +0900

    Image追加

--- Assets/Scripts/Ito/Manager/GameManager.cs
+++ Assets/Scripts/Ito/Manager/GameManager.cs
@@ -1,31 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
-    public Transform SavePoint => _savePoint;
+    public Transform[] SavePoints => _savePoints;
     public int SavePointIndex => _savePointIndex;
     public float Timer => _timer;
 
-    [SerializeField] private Transform _savePoint;
+    [SerializeField] private Transform[] _savePoints;
+    [SerializeField] private GameObject _gameOverUi;
+    [SerializeField] private string _playerTag = "Player";
+    [SerializeField] private Text _timerText;
+    private GameObject _player;
     private int _savePointIndex = 0;
     private float _timer;
 
-
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag(_playerTag);
+        if(!_player)
+        {
+            Debug.LogError("プレイヤーを参照できませんでした");
+        }
+    }
     private void Update()
     {
         _timer -= Time.deltaTime;
+        _timerText.text = _timer.ToString("F0");
+    }
+
+    void SavePointUpdate(int savePointIndex)
+    {
+        _savePointIndex = savePointIndex;
     }
 
     public void TimeOver()
     {
-
+        _player.SetActive(false);
+        _gameOverUi.SetActive(true);
     }
 
     public void Respawn()
     {
-
+        _player.transform.position = _savePoints[_savePointIndex].position;
     }
 
 }
